f90955ad52b3f7b5b94c5484c1265c7289f24e0e
hongsh429 <shh0429@kakao.com>
2023-07-21 14:21:03 +0900

    first commit

---
+++ src/main/java/com/project/ohouclonecoding/OhouCloneCodingApplication.java
@@ -0,0 +1,13 @@
+package com.project.ohouclonecoding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OhouCloneCodingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OhouCloneCodingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/ohouclonecoding/OhouCloneCodingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ohouclonecoding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OhouCloneCodingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
