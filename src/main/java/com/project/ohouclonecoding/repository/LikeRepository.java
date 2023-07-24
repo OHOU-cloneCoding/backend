6c1d6dc2b2b329f363b08ce9052109cbb9b0b586
s2cocos2 <yewon0309@naver.com>
2023-07-24 11:54:08 +0900

    feat : 댓글 좋아요 생성

--- src/main/java/com/project/ohouclonecoding/repository/LikeRepository.java
+++ src/main/java/com/project/ohouclonecoding/repository/LikeRepository.java
@@ -1,5 +1,6 @@
 package com.project.ohouclonecoding.repository;
 
+import com.project.ohouclonecoding.entity.Comment;
 import com.project.ohouclonecoding.entity.Like;
 import com.project.ohouclonecoding.entity.Post;
 import com.project.ohouclonecoding.entity.User;
@@ -10,4 +11,7 @@ import java.util.Optional;
 public interface LikeRepository extends JpaRepository<Like, Long> {
     boolean existsLikeByPostAndUser(Post post, User user);
     Optional<Like> findByPostAndUser(Post post, User user);
+
+    boolean existsLikeByCommentAndUser(Comment comment, User user);
+    Optional<Like> findByCommentAndUser(Comment comment, User user);
 }
